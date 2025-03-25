25439af59dfb6fe5e63e30e7a9e8f1ac6e8991da
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2025-03-25 19:49:36 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = new int[9];
+
+        int max = 0;
+        int index = 0;
+
+        for (int i = 0; i < 9; i++) {
+            numbers[i] = Integer.parseInt(br.readLine());
+
+            if(numbers[i] > max){
+                max = numbers[i];
+                index = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index + 1);
+    }
+}
