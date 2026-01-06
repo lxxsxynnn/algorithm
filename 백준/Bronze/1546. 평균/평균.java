507216e007441882143670c19c597f8f0dcb7baf
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-01-06 21:26:51 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,20 +1,21 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] scores = new int[N];
 
         // 배열 초기화
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++) {
-            scores[i] = sc.nextInt();
+            scores[i] = Integer.parseInt(st.nextToken());
         }
-
-        int idx = 0;    //최댓값 인덱스
-
-        // 최댓값 구하기
+        
         /* 1 2 3 4 5 -> 5개의 숫자가 있음
          * 첫 번째 수와 두 번째 수 비교 -> 최대값 인덱스에 1 저장
          * 두 번째 수와 세 번쨰 수 비교 -> 최대값 인덱스에 2 저장
@@ -23,15 +24,15 @@ public class Main {
          * 최댓값 인덱스는 0부터 시작
          * 비교대상은 0 + 1 부터 마지막까지
          * */
+        // 인덱스를 가지고 최댓값 구하기
+        int M = scores[0];
+        
         for (int i = 1; i < scores.length; i++) {
-            if(scores[i] > scores[idx]) {
-                idx = i;
+            if(scores[i] > M) {
+                M = scores[i];
             }
         }
-
-        // 인덱스를 가지고 최댓값 구하기
-        int M = scores[idx];
-
+        
         // 총합을 저장할 변수 선언
         double sum = 0;
 
