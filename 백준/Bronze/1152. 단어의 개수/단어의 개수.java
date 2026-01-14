2d3efeda6a59409d66bbf2b930d01554e7cb6438
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-01-14 12:55:55 +0900

    [Bronze II] Title: 단어의 개수, Time: 428 ms, Memory: 30912 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -3,18 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine().trim();
+        String s = sc.nextLine();
         int n = 0;
+        boolean inWord = false;
 
-        if (s.isEmpty()) {
-            System.out.println(n);
-            return;
-        }
-
-        n = 1;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ') {
-                n++;
+                inWord = false;
+            } else if (!inWord && s.charAt(i) != ' ') {
+                inWord = true;
+                n ++;
             }
         }
 
