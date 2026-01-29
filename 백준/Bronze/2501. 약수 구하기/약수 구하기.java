0102466980567198a4198c6ab009569105c799ca
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-01-29 11:38:08 +0900

    [Bronze III] Title: 약수 구하기, Time: 176 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int order = 0;
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) order++;
+            if(order == K) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+        System.out.println(0);
+    }
+}
