3200b3815dc21501df31761b83874e93e058beff
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-02-10 20:37:23 +0900

    [Silver IV] Title: 제로, Time: 180 ms, Memory: 23472 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        while(K-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0){
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        for(int num : stack) {
+            sum += num;
+        }
+
+        System.out.println(sum);
+    }
+}
