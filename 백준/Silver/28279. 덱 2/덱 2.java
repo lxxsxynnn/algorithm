4cc46ce54aa83c1b921086eead162abf67e5d674
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-02-13 22:14:50 +0900

    [Silver IV] Title: 덱 2, Time: 732 ms, Memory: 251032 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
@@ -17,12 +19,24 @@ public class Main {
             switch(command){
                 case 1: deque.addFirst(Integer.parseInt(st.nextToken())); break;
                 case 2: deque.addLast(Integer.parseInt(st.nextToken())); break;
-                case 3: sb.append(!deque.isEmpty()? deque.pollFirst() : -1).append("\n"); break;
-                case 4: sb.append(!deque.isEmpty()? deque.pollLast() : -1).append("\n"); break;
+                case 3:
+                    Integer val = deque.pollFirst();
+                    sb.append(val == null? -1: val).append("\n");
+                    break;
+                case 4:
+                    val = deque.pollLast();
+                    sb.append(val == null? -1 : val).append("\n");
+                    break;
                 case 5: sb.append(deque.size()).append("\n"); break;
                 case 6: sb.append(deque.isEmpty()? 1 : 0).append("\n"); break;
-                case 7: sb.append(!deque.isEmpty()? deque.peekFirst() : -1).append("\n"); break;
-                case 8: sb.append(!deque.isEmpty()? deque.peekLast() : -1).append("\n"); break;
+                case 7:
+                    val = deque.peekFirst();
+                    sb.append(val == null? -1 : val).append("\n");
+                    break;
+                case 8:
+                    val = deque.peekLast();
+                    sb.append(val == null? -1: val).append("\n");
+                    break;
             }
         }
         System.out.print(sb);
