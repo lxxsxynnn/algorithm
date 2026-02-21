4a272d143bc95ab809573cd73aa3a0a832ce85fe
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-02-21 20:48:23 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.print(factorial(N));
+    }
+
+    static long factorial(int N){
+        if(N == 0) return 1;
+        return N * factorial(N - 1);
+    }
+}
