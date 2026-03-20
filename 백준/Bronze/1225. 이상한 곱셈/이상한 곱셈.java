8b41289e9c4e715dec6802d56cebe09d03db80ef
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-03-20 17:55:30 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        long sumA = 0;
+        long sumB = 0;
+        
+        for (int i = 0; i < A.length(); i++) {
+            sumA += A.charAt(i) - '0';
+        }
+        
+        for (int i = 0; i < B.length(); i++) {
+            sumB += B.charAt(i) - '0';
+        }
+
+        System.out.println(sumA * sumB);
+    }
+}
