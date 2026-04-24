b0157c715427fb7b08474b69a602c9e7343e2551
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-04-24 20:05:30 +0900

    Title: 시험 통과 여부 확인하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260424/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 80) System.out.println("pass");
+        else System.out.println(80 - N + " more score");
+    }
+}
