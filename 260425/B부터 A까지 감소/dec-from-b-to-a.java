89e51f7e056f6de99a11ad56ac594fc2c5ef6c23
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-04-25 23:35:28 +0900

    Title: B부터 A까지 감소, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        for(int i = B; i >= A; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
