0b59442c801e1601b4848f6d44b2512ceadf8446
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-04-25 23:13:35 +0900

    Title: 살 수 있는 물건, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260425/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 3000) System.out.println("book");
+        else if(N >= 1000) System.out.println("mask");
+        else System.out.println("no");
+    }
+}
