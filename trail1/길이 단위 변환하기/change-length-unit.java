642f69f5f40457545915dc3f5836ece630abc59c
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-09 11:48:57 +0900

    [Passed] 길이 단위 변환하기 (97ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double f = 9.2;
+        double m = 1.3;
+
+        System.out.printf("%.1fft = %.1fcm\n%.1fmi = %.1fcm",
+                            f, f * 30.48, m, m * 160934);
+    }
+}
