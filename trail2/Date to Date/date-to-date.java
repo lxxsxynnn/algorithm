80ba79950e2a6c7faad99848150254c579c420f5
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-13 15:42:20 +0900

    [Passed] Date to Date (126ms, 10MB)

---
+++ trail2/Date to Date/date-to-date.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int[] months = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int days = 1;
+
+        while(true){
+            if(m1 == m2 && d1 == d2) break;
+            d1++;
+            days++;
+
+            if(d1 > months[m1]){
+                m1++;
+                d1 = 1;
+            }
+        }
+
+        System.out.println(days);
+    }
+}
