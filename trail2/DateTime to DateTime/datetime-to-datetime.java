8f10dd9facd7adb3323ca39555ebdc3d6781e70b
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-13 15:49:39 +0900

    [Passed] DateTime to DateTime (125ms, 10MB)

---
+++ trail2/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int start = 11 * 24 * 60 + 11 * 60 + 11;
+        int end = A * 24 * 60 + B * 60 + C;
+
+        int result = end - start;
+
+        if (result < 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result);
+        }
+    }
+}
