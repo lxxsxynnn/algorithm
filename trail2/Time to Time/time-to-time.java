d2f2fd88de0b5672cc6621f7b558e036fe5bffd4
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-13 15:26:43 +0900

    [Passed] Time to Time (119ms, 10MB)

---
+++ trail2/Time to Time/time-to-time.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+        int min = 0;
+        
+        while(true){
+            if(a == c && b == d) break;
+
+            b++;
+            min++;
+
+            if(b == 60) {
+                a++;
+                b = 0;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
