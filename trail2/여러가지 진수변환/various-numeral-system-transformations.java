7962aa37e17139454da1adfe13a5fa169632f072
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-15 16:31:45 +0900

    [Passed] 여러가지 진수변환 (86ms, 9MB)

---
+++ trail2/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+
+        while(N > 0){
+            sb.append(N % B);
+            N /= B;
+        }
+
+        System.out.println(sb.reverse());
+    }
+}
