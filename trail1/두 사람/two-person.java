75712a4035fb1bb7ae20743a081e6cde2b216b67
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-20 20:50:47 +0900

    [Passed] 두 사람 (152ms, 10MB)

---
+++ trail1/두 사람/two-person.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ageA = sc.nextInt();
+        String genA = sc.next();
+        int ageB = sc.nextInt();
+        String genB = sc.next();
+        
+        if((ageA >= 19 && genA.equals("M"))||(ageB >= 19 && genB.equals("M"))) System.out.println(1);
+        else System.out.println(0);
+    }
+}
