e927c5fffc66b220ac1b85aefbda650125d430d4
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-20 18:15:20 +0900

    [Passed] 4번의 크기 비교 (127ms, 10MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+        int E = sc.nextInt();
+
+        System.out.println((A > B) ? 1 : 0);
+        System.out.println((A > C) ? 1 : 0);
+        System.out.println((A > D) ? 1 : 0);
+        System.out.println((A > E) ? 1 : 0);
+    }
+}
