43bcad41e0f3f9e71497c3b5e406d2ffd5a52d57
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-21 13:55:57 +0900

    [Passed] 홀수만 출력 (127ms, 10MB)

---
+++ trail1/홀수만 출력/output-only-odd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        for(int i = A; i <= B; i++){
+            if(i % 2 == 0) continue;
+            System.out.print(i + " ");
+        }
+    }
+}
