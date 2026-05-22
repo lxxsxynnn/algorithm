7e538b789314d309d141829071b6fe373c488180
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-22 16:37:36 +0900

    [Passed] 증가시키며 출력하기 2 (89ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int n = 10;
+
+        while(n < 27){
+            System.out.print(n + " ");
+            n++;
+        }
+    }
+}
