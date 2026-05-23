9fde12409e1a0e2a12ea500303037e83a7afe91e
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-23 20:25:56 +0900

    [Passed] 문자 출력하기 (118ms, 10MB)

---
+++ trail1/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int N = 8;
+
+        while(N-- > 0){
+            System.out.print(str);
+        }
+    }
+}
