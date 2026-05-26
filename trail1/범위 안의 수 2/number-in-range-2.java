b28fdaa2238a115cb1ad47eda367c92cd2f714a8
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-26 14:03:29 +0900

    [Passed] 범위 안의 수 2 (143ms, 10MB)

---
+++ trail1/범위 안의 수 2/number-in-range-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < 10; i++){
+            int num = sc.nextInt();
+
+            if(num >= 0 && num <= 200){
+                sum += num;
+                cnt++;
+            }
+        }
+
+        System.out.printf("%d %.1f", sum, (double) sum / cnt);
+    }
+}
