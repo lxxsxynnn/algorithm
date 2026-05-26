6c5aedd2544500e359efd58728d6a1b7ea763bdd
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-26 14:42:37 +0900

    [Passed] 총합 구하기 (131ms, 10MB)

---
+++ trail1/총합 구하기/find-the-total.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int sum = 0;
+        
+        for(int i = A; i <= B; i++){
+            if(i % 6 == 0 && i % 8 != 0){
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
