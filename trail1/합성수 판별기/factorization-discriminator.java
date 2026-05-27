e68987858e1f83e87d8a10c2a7f241a2c1f666df
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-27 16:45:03 +0900

    [Passed] 합성수 판별기 (143ms, 10MB)

---
+++ trail1/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 2; i <= (N - 1); i++){
+            if(N % i == 0){
+                System.out.println("C");
+                return;
+            }
+        }
+
+        System.out.println("N");
+    }
+}
