78792a4cd6dad77dc4a717ac5b79053e8b0fb9aa
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-27 15:00:30 +0900

    [Passed] 특정 조건의 온전수 구하기 (129ms, 10MB)

---
+++ trail1/특정 조건의 온전수 구하기/print-onjeonsu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++){
+            if(i % 2 == 0 
+            || (i % 3 == 0 && i % 9 != 0) 
+            || (i % 5 == 0 && i % 10 != 0)) continue;
+            System.out.print(i + " ");
+        }
+    }
+}
