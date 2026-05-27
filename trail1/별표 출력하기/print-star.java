0e96e4bdd856a878bd9f3c0990d4f90d9cd3903d
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-27 17:27:51 +0900

    [Passed] 별표 출력하기 (145ms, 10MB)

---
+++ trail1/별표 출력하기/print-star.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= 2 * N - 1; i++) {
+
+            int star;
+
+            if (i <= N) {
+                star = i;
+            } else {
+                star = 2 * N - i;
+            }
+
+            for (int j = 0; j < star; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
