c156ccdf262d65be92fdf4ea682fd69b400e0c8c
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-27 16:42:27 +0900

    [Passed] ab 사이에 있는 c (129ms, 10MB)

---
+++ trail1/ab 사이에 있는 c/c-between-a-and-b.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        for(int i = a; i <= b; i++){
+            if(i % c == 0){
+                System.out.println("YES");
+                return;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
