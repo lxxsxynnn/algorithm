d2c342db37ff08fa3dba1e0fa4dce33b2a234c59
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-27 14:49:04 +0900

    [Passed] 주어진 수까지의 곱 (123ms, 10MB)

---
+++ trail1/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int prod = 1;
+
+        for(int i = A; i <= B; i++){
+            prod *= i;
+        }
+
+        System.out.println(prod);
+    }
+}
