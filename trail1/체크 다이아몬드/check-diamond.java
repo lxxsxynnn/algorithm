920b9be29f42c05736e24457c0fb97da17cb4748
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-28 23:57:16 +0900

    [Passed] 체크 다이아몬드 (135ms, 10MB)

---
+++ trail1/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < 2 * N - 1; i++) {
+            for (int j = 0; j < 2 * N - 1; j++) {
+            if (Math.abs(N - 1 - i) + Math.abs(N - 1 - j) <= N - 1
+                && (i + j) % 2 == (N - 1) % 2) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
