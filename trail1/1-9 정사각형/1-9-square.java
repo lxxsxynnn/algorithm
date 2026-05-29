4822781aa0149666d74939cf5cb0dcbc7429074b
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-29 16:55:05 +0900

    [Passed] 1-9 정사각형 (140ms, 10MB)

---
+++ trail1/1-9 정사각형/1-9-square.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 1;
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(cnt == 10) cnt = 1;
+                System.out.print(cnt++);
+            }
+            System.out.println();
+        }
+    }
+}
