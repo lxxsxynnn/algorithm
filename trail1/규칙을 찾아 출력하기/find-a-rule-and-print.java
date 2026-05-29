38b586a03887f7cb5fe5633483761970595956b0
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-29 15:21:07 +0900

    [Passed] 규칙을 찾아 출력하기 (134ms, 10MB)

---
+++ trail1/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = 0; i < N; i ++){
+            for(int j = 0; j < N; j++){
+                if(i == 0 || j == N - 1 || i > j) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
