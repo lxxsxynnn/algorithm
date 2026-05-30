7362ca4f61546fe2ee9b2449c8a7eadebedacd2d
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-31 00:19:35 +0900

    [Passed] 구구단으로 이루어진 직각 삼각형 (125ms, 10MB)

---
+++ trail1/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N - i + 1; j++){
+                System.out.print(i + " * " + j + " = " + (i * j));
+                if(j != N - i + 1) System.out.print(" / ");
+            }
+            System.out.println();
+        }
+    }
+}
