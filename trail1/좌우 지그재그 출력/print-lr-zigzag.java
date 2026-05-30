2eccc061540f6abb1e06b8fccb9de01503112efd
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-05-30 22:51:34 +0900

    [Passed] 좌우 지그재그 출력 (202ms, 12MB)

---
+++ trail1/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int cnt = 1;
+
+        for(int i = 0; i < N; i++){
+            if(i % 2 == 0){
+                for(int j = 1; j <= N; j++){
+                    System.out.print(cnt++ + " ");
+                }
+            } else {
+                for(int j = N; j > 0; j--){
+                    System.out.print(cnt + j - 1 + " ");
+                }
+                cnt += N;
+            }
+            System.out.println();
+        }
+    }
+}
