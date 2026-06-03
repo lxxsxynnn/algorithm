e9d2041dd3975e3da34dfe23641bdbc59a996eef
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-03 22:57:54 +0900

    [Passed] 10개 입력받기 (134ms, 10MB)

---
+++ trail1/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nums = new int[10];
+        int sum = 0;
+
+        for(int i = 0; i < 10; i++){
+            int n = sc.nextInt();
+            if(n == 0) {
+                System.out.printf("%d %.1f", sum, (double)sum / i);
+                return;
+            }
+            nums[i] = n;
+            sum += nums[i];
+        }
+
+        System.out.printf("%d %.1f", sum, (double)sum / 10);
+    }
+}
