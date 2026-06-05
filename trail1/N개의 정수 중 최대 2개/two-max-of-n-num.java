ef4063df642874364a236045591663542d0ee746
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-05 15:09:40 +0900

    [Passed] N개의 정수 중 최대 2개 (163ms, 10MB)

--- trail1/N개의 정수 중 최대 2개/two-max-of-n-num.java
+++ trail1/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -12,16 +12,18 @@ public class Main {
             nums[i] = sc.nextInt();
         }
 
-        for(int i = 0; i < N - 1; i++){
-            for(int j = i + 1; j < N; j++){
-                if(nums[i] < nums[j]){
-                    int tmp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = tmp;
-                }
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for(int i = 0; i < N; i++){
+            if(nums[i] > max1){
+                max2 = max1;
+                max1 = nums[i];
+            } else if(nums[i] > max2){
+                max2 = nums[i];
             }
         }
 
-        System.out.print(nums[0] + " " + nums[1]);
+        System.out.print(max1 + " " + max2);
     }
 }
