3e5fa1670f12e921c9f0fa03718a99a555ed3261
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-05 13:19:59 +0900

    [Passed] 가장 큰 수 고르기 (147ms, 10MB)

---
+++ trail1/가장 큰 수 고르기/picking-biggest-number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < 10; i++){
+            int num = sc.nextInt();
+
+            if(num > max) max = num;
+        }
+
+        System.out.println(max);
+    }
+}
