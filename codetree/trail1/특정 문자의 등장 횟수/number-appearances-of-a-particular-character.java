c1e89e547ce5eb1784db1448b8cd8c45466b92cc
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-19 12:29:30 +0900

    [Passed] 특정 문자의 등장 횟수 (128ms, 10MB)

---
+++ codetree/trail1/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        int eeCnt = 0;
+        int ebCnt = 0;
+
+        for(int i = 0; i < str.length() - 1; i++){
+            if(str.substring(i, i + 2).equals("ee")){
+                eeCnt++;
+            };
+        }
+
+        for(int i = 0; i < str.length() - 1; i++){
+            if(str.substring(i, i + 2).equals("eb")){
+                ebCnt++;
+            };
+        }
+
+        System.out.print(eeCnt + " " + ebCnt);
+    }
+}
