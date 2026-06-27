523208d868533041f1b5bcd86382877bf5b4b94b
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-27 16:24:42 +0900

    [Passed] 정수만 추출하기 (123ms, 10MB)

---
+++ codetree/trail1/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.next();
+        String b = sc.next();
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) < '0' || a.charAt(i) > '9') break;
+            sb1.append(a.charAt(i));
+        }
+
+        for(int i = 0; i < b.length(); i++){
+            if(b.charAt(i) < '0' || b.charAt(i) > '9') break;
+            sb2.append(b.charAt(i));
+        }
+
+        System.out.print(Integer.parseInt(sb1.toString()) + Integer.parseInt(sb2.toString()));
+    }
+}
