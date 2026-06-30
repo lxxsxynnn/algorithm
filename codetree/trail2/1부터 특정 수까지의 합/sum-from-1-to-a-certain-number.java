239b44988f406c8d08778f9be03199e313f49cdc
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-06-30 10:56:12 +0900

    [Passed] 1부터 특정 수까지의 합 (132ms, 10MB)

---
+++ codetree/trail2/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(getSum(n));
+    }
+
+    public static int getSum(int n){
+        return (n * (n + 1) / 2) / 10;
+    }
+}
