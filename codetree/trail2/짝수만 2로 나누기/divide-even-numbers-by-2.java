01556e60ed8f2b52252802d0e653505c1fc26fc5
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-07-08 16:46:24 +0900

    [Passed] 짝수만 2로 나누기 (137ms, 10MB)

---
+++ codetree/trail2/짝수만 2로 나누기/divide-even-numbers-by-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        evenDivisor(arr);
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void evenDivisor(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] % 2 == 0) arr[i] /= 2;
+        }
+    }
+}
