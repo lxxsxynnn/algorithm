0f5956c0df4b0eae9e932385cb56027de760bf92
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-07-08 17:44:00 +0900

    [Passed] 절댓값으로 변경 (150ms, 14MB)

---
+++ codetree/trail2/절댓값으로 변경/find-the-absolute-value.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        applyAbs(arr);
+
+        for(int num : arr){
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void applyAbs(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Math.abs(arr[i]);
+        }
+    }
+}
