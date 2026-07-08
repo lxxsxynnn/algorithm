af311b51cf3f49e08a333134cf69f013b578d78f
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-07-08 16:20:36 +0900

    [Passed] 두 정수 값 교환하기 (127ms, 10MB)

---
+++ codetree/trail2/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        IntWrapper n = new IntWrapper(sc.nextInt());
+        IntWrapper m = new IntWrapper(sc.nextInt());
+
+        swap(n, m);
+
+        System.out.println(n.value + " " + m.value);
+    }
+
+    public static void swap(IntWrapper n, IntWrapper m){
+        int tmp = n.value;
+        n.value = m.value;
+        m.value = tmp;
+    }
+}
+
+class IntWrapper{
+    int value;
+
+    public IntWrapper(int value){
+        this.value = value;
+    }
+}
