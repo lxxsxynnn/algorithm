8418314751a3e42529fa7db96ba22c5cbf807dc0
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-07-18 19:43:06 +0900

    [Passed] 100으로 나눈 나머지의 수열 (125ms, 10MB)

---
+++ codetree/trail2/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(f(n));
+    }
+
+    public static int f(int n){
+        if(n == 1) return 2;
+        else if(n == 2) return 4;
+        else return (f(n - 1) * f(n - 2)) % 100;
+    }
+}
