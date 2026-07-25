b13559bdd354dbfceb0e5b0a78a9ad70c987e47d
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-07-25 14:16:08 +0900

    [Passed] 두 개의 동일한 수열 (130ms, 10MB)

---
+++ codetree/trail2/두 개의 동일한 수열/two-equal-series.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] A = new int[n];
+        int[] B = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            B[i] = sc.nextInt();
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        boolean same = true;
+
+        for (int i = 0; i < n; i++) {
+            if (A[i] != B[i]) {
+                same = false;
+                break;
+            }
+        }
+
+        System.out.println(same ? "Yes" : "No");
+    }
+}
