a1e11bb60c62c2257add50075cb33675e9234025
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-08-10 19:43:55 +0900

    [Passed] 연속되는 수 2 (127ms, 10MB)

--- codetree/trail2/연속되는 수 2/continuous-number2.java
+++ codetree/trail2/연속되는 수 2/continuous-number2.java
@@ -4,21 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int cnt = 0;
-        int max = 0;
-        int[] arr = new int[n];
+        int prev = sc.nextInt();
+        int cnt = 1;
+        int max = 1;
 
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
+        for(int i = 1; i < n; i++){
+            int curr = sc.nextInt();
 
-            if(i == 0 || arr[i] == arr[i - 1]) cnt++;
-            else {
-                max = Math.max(max, cnt);
-                cnt = 1;
-            }
+            if(prev == curr) cnt++;
+            else cnt = 1;
+
+            max = Math.max(max, cnt);
+            prev = curr;
         }
-
-        max = Math.max(max, cnt);
 
         System.out.print(max);
     }
