cb25b1725d325f2f43974d42c9f0678a5263c567
lxxsxynnn <lxxsxynnn@khu.ac.kr>
2026-08-10 20:19:00 +0900

    [Passed] 연속되는 수 3 (126ms, 10MB)

---
+++ codetree/trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int prev = sc.nextInt();
+        int cnt = 1;
+        int max = 1;
+
+        for(int i = 1; i < n; i++){
+            int curr = sc.nextInt();
+
+            if(prev * curr > 0) cnt++;
+            else cnt = 1;
+
+            max = Math.max(max, cnt);
+            prev = curr;
+        }
+
+        System.out.print(max);
+    }
+}
